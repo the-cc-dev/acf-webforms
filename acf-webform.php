4e3abe876c24383797a6b90e0dcf368f50d3e98e
Joel Milne <joel@goldhat.ca>
2016-10-01 01:59:09 +0300

    draft main plugin file

---
+++ acf-webform.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+  Plugin Name: ACF Webforms
+  Plugin URI: http://goldhat.ca/plugins/acf-webforms
+  Description: Create webforms with ACF field groups
+  Version: 1.0.0
+  Author: Joel Milne, GoldHat Group
+  Author URI: http://goldhat.ca
+  Text Domain: acf-webforms
+  License: GPLv2 or later
+ */
+
+new ACF_Webforms;
+
+class ACF_Webforms {
+
+
+
+}
+
+/*
+
+Inject acf_form_header() into the head
